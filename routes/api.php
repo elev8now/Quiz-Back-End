15b249b57597c80d922268706c4f8a643fe2f3c8
Jonathan Smithies <jonathan@elev8now.co.uk>
2019-09-18 12:52:34 +0100

    passport/authentication completed

--- routes/api.php
+++ routes/api.php
@@ -19,18 +19,18 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 $router->group(["prefix" => "questions"], function ($router) {
     // create a new question
-    $router->post("/", "Questions@store");
+    $router->post("/", "Questions@store")->middleware('auth:api');
     // get a list of all questions
     $router->get("/", "Questions@index");
     // get a specific question by id 
     // {question} is a url parameter representing the id we want
     $router->get("{question}", "Questions@show");
     // edit a question by id
-    $router->put("{question}", "Questions@update");
+    $router->put("{question}", "Questions@update")->middleware('auth:api');
     // delete a question by id 
-    $router->delete("{question}", "Questions@destroy");
+    $router->delete("{question}", "Questions@destroy")->middleware('auth:api');
     // add a new answer to a question
-    $router->post("{question}/answers", "Answers@store");
+    $router->post("{question}/answers", "Answers@store")->middleware('auth:api');
     // get all answers to a question
     $router->get("{question}/answers", "Answers@index");
 });
@@ -40,8 +40,8 @@ $router->group(["prefix" => "answers"], function ($router) {
     // {question} is a url parameter representing the id we want
     $router->get("{answer}", "Answers@show");
     // edit a specific answer by id
-    $router->put("{answer}", "Answers@update");
+    $router->put("{answer}", "Answers@update")->middleware('auth:api');
     // delete a specific answer by id 
-    $router->delete("{answer}", "Answers@destroy");
+    $router->delete("{answer}", "Answers@destroy")->middleware('auth:api');
 
 });
